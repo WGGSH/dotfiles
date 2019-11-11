916cb3b7f5393c166ef43e89b58eb0abace2c0fc
naoki <naokipgt@gmail.com>
2019-11-11 20:11:10 +0900

    リンク貼るスクリプト書いた

---
+++ vimlink.bat
@@ -0,0 +1,8 @@
+del C:\Users\naoki\AppData\Local\nvim\dein.toml
+mklink /H C:\Users\naoki\AppData\Local\nvim\dein.toml dein.toml
+del C:\Users\naoki\AppData\Local\nvim\dein_lazy.toml
+mklink /H C:\Users\naoki\AppData\Local\nvim\dein_lazy.toml dein_lazy.toml
+del C:\Users\naoki\AppData\Local\nvim\init.vim
+mklink /H C:\Users\naoki\AppData\Local\nvim\init.vim init.vim
+del C:\Users\naoki\AppData\Local\nvim\oni\config.vim
+mklink /H C:\Users\naoki\AppData\Local\nvim\oni\config.vim oni\config.vim

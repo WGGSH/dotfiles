5f3d3c037a87de6658859a52a58371a1fb514895
wgg <is0332ks@ed.ritsumei.ac.jp>
2020-01-19 01:47:16 +0900

    GUI関連の設定追加

--- ginit.vim
+++ ginit.vim
@@ -1,7 +1,10 @@
-set Guifont=Cica:h20
-set showtabline=1
+set showtabline=2
 set guioptions-=e
 
 if exists("g:gui_oni")
   let g:airline_theme = 'behelit'
 endif
+
+
+Guifont! Cica:h20
+call GuiWindowFullscreen(1)
--- vimlink.bat
+++ vimlink.bat
@@ -4,6 +4,8 @@ del C:\Users\naoki\AppData\Local\nvim\dein_lazy.toml
 mklink /H C:\Users\naoki\AppData\Local\nvim\dein_lazy.toml dein_lazy.toml
 del C:\Users\naoki\AppData\Local\nvim\init.vim
 mklink /H C:\Users\naoki\AppData\Local\nvim\init.vim init.vim
+del C:\Users\naoki\AppData\Local\nvim\ginit.vim
+mklink /H C:\Users\naoki\AppData\Local\nvim\ginit.vim ginit.vim
 del C:\Users\naoki\AppData\Local\nvim\coc-settings.json
 mklink /H C:\Users\naoki\AppData\Local\nvim\coc-settings.json coc-settings.json
 del C:\Users\naoki\AppData\Local\nvim\oni\config.vim

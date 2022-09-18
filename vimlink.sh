8ee0eb4fdfd7645c0e7d428848176cd826267f8d
naoki <naokipgt@gmail.com>
2022-09-18 21:51:15 +0900

    vim の初期化スクリプト調整

--- vimlink.sh
+++ vimlink.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+mkdir -p ~/..config/nvim/
+
 unlink ~/.config/nvim/init.vim
 unlink ~/.config/nvim/ginit.vim
 unlink ~/.config/nvim/dein.toml
 unlink ~/.config/nvim/dein_lazy.toml
-unlink ~/.config/nvim/oni/config.vim
-unlink ~/.config/nvim/coc-settings.json
-unlink ~/.config/oni/config.tsx
+# unlink ~/.config/nvim/oni/config.vim
+# unlink ~/.config/nvim/coc-settings.json
+# unlink ~/.config/oni/config.tsx
 
 ln -s `pwd`/init.vim ~/.config/nvim/
 ln -s `pwd`/ginit.vim ~/.config/nvim/
 ln -s `pwd`/dein.toml ~/.config/nvim/
 ln -s `pwd`/dein_lazy.toml ~/.config/nvim/
-ln -s `pwd`/oni/config.vim ~/.config/nvim/oni/
-ln -s `pwd`/coc-settings.json ~/.config/nvim/
-ln -s `pwd`/oni/config.tsx ~/.config/oni/
+# ln -s `pwd`/oni/config.vim ~/.config/nvim/oni/
+# ln -s `pwd`/coc-settings.json ~/.config/nvim/
+# ln -s `pwd`/oni/config.tsx ~/.config/oni/
